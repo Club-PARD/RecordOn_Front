cf4b54ca3861faeb3df56ed3f2e284a0bc33e42f
Woochal <parkwoochal@gmail.com>
2024-06-23 21:52:21 +0900

    주영 : 초기 환경설정

---
+++ src/Router/Router.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function Router() {
+    return (
+        <Routes>
+            {/* <Route path='/' element={<MainPage />} /> */}
+        </Routes>
+    );
+}
+
+export default Router;
