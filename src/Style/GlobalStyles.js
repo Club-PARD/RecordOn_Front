7d3359388bd0bd63a0026e1e6c826fa9c9e8880a
Woochal <parkwoochal@gmail.com>
2024-07-02 16:48:56 +0900

    240702 주영 : 폰트 추가

--- src/Style/GlobalStyles.js
+++ src/Style/GlobalStyles.js
@@ -40,6 +40,7 @@ ol[role='list'] {
 /* 핵심 body의 기본값을 설정합니다. */
 body {
   min-height: 100vh;
+  font-family: "Pretendard";
   /* line-height: 1.5; */
 }
 
@@ -86,8 +87,6 @@ textarea:not([rows]) {
 // 웹폰트 적용 (내장 폰트를 적용할 경우 개발 환경에 따라 적용이 안되는 경우가 있음)
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 100;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.woff2') format('woff2'),
@@ -97,8 +96,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 200;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.woff2') format('woff2'),
@@ -108,8 +105,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 300;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.woff2') format('woff2'),
@@ -119,8 +114,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 400;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.woff2') format('woff2'),
@@ -130,8 +123,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 500;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.woff2') format('woff2'),
@@ -141,8 +132,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 600;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.woff2') format('woff2'),
@@ -152,8 +141,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 700;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.woff2') format('woff2'),
@@ -163,8 +150,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 800;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.woff2') format('woff2'),
@@ -174,8 +159,6 @@ textarea:not([rows]) {
 }
 @font-face {
   font-family: 'Pretendard';
-  font-weight: 900;
-  font-style: normal;
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.eot');
   src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.eot?#iefix') format('embedded-opentype'),
       url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.woff2') format('woff2'),
