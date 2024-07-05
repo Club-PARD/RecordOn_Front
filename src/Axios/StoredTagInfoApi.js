1e3ecdc10cb538edf15f0c67cb56c6df1c8cea55
KKIMYENN <yenny031003@gmail.com>
2024-07-05 13:59:32 +0900

    예은: 어디까지 했더라

--- src/Axios/StoredTagInfoApi.js
+++ src/Axios/StoredTagInfoApi.js
@@ -8,6 +8,7 @@ export const getAllTagAndQuestionAPI = async () => {
     console.log (requestUrl);
   try {
     const response = await axios.get(requestUrl);
+    console.log (response.data);
     return response.data;
   } catch (error) {
     console.error(error);
