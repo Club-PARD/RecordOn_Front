ec222d89845f6846a07cadbd72bfb4db26758eae
Mingyu Lee <livingdavid@naver.com>
2024-07-10 15:03:01 +0900

    240710 민규 : 헬멧 + 오픈그래프 수정

--- src/Pages/Home/Components/RegisterModal.js
+++ src/Pages/Home/Components/RegisterModal.js
@@ -9,7 +9,7 @@ import { ReactComponent as Profile } from "../../../Assets/Profile.svg";
 import DropdownJob from "./DropdownJob.js";
 import { registerUserAPI } from '../../../Axios/RegisterApi.js'
 import { useNavigate } from 'react-router-dom';
-import PrivacyPolicyModal from './privacyPolicyModal.js';
+import PrivacyPolicyModal from './PrivacyPolicyModal.js';
 import ServicePolicyModal from './ServicePolicyModal.js';
 
 
